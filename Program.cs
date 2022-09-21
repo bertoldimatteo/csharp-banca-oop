a7d12209071c11bf7e8a456b3f9c767dd4898d18
bertoldimatteo <bertoldi.matteo660@gmail.com>
2022-09-21 16:06:26 +0200

    aggiunto ricerca cliente

--- Program.cs
+++ Program.cs
@@ -28,10 +28,39 @@ class Bank
         loans = new List<Loans>();  
     }
 
-    public void addUser(Clients client)
+    public void addClient(Clients client)
     {
         clients.Add(client);
     }
+
+    public void editClient(Clients client, string name, string surname, int salary)
+    {
+        client.Name = name;
+        client.Surname = surname;
+        client.Salary = salary;
+    }
+
+    public void addLoans(Loans loan)
+    {
+        loans.Add(loan);
+    }
+
+    public List<Clients> searchClient(int id, string name, string surname)
+    {
+        List<Clients> results = new List<Clients>();
+
+        foreach (Clients client in clients)
+        {
+            if(client.Id + client.Name + client.Surname == id + name + surname)
+            {
+                results.Add(client);    
+            }
+        }
+        return results;
+    }
+        
+        
+        
 }
 
 class Clients
