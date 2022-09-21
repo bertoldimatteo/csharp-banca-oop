14987565208750fad05f51f2ba34c98a71c168d5
bertoldimatteo <bertoldi.matteo660@gmail.com>
2022-09-21 15:44:35 +0200

    aggiunta struttura banca iniziale + aggiunta S a Client per il maledetto ADD

--- Program.cs
+++ Program.cs
@@ -1,8 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Client user1 = new Client("Mario", "Rossi", "AAAAA", 25000.00);
-Client user2 = new Client("Paolo", "Biondi", "BBBBB", 17000.10);
-Client user3 = new Client("Carlo", "Neri", "CCCCC", 4900.80);
+Clients user1 = new Clients("Mario", "Rossi", "AAAAA", 25000.00);
+Clients user2 = new Clients("Paolo", "Biondi", "BBBBB", 17000.10);
+Clients user3 = new Clients("Carlo", "Neri", "CCCCC", 4900.80);
 
 Console.WriteLine($"ID cliente {user1.Id} Nome cliente {user1.Name}");
 Console.WriteLine($"ID cliente {user2.Id} Nome cliente {user2.Name}");
@@ -14,13 +14,27 @@ Loans loan3 = new Loans(user3, 5000, 106.34, 2022);
 class Bank
 {
     private string name;
+    public string Name { get; set; }
 
-    //un insieme di clienti (una lista)
+    List<Clients> clients;
 
-    //un insieme di prestiti concessi ai clienti (una lista)
+    List<Loans> loans;
+
+    public Bank(string name)
+    {
+        this.Name = name;
+
+        clients = new List<Clients>();
+        loans = new List<Loans>();  
+    }
+
+    public void addUser(Clients client)
+    {
+        clients.Add(client);
+    }
 }
 
-class Client
+class Clients
 {
     private int id;
     private string name;
@@ -37,13 +51,13 @@ class Client
     public string FiscalCode { get; set; }
     public double Salary { get; set; }
     
-    public Client(string name, string surname, string fiscalCode, double salary)
+    public Clients(string name, string surname, string fiscalCode, double salary)
     {
         this.Name = name;
         this.Surname = surname;
         this.FiscalCode = fiscalCode;   
         this.Salary = salary;
-        this.Id = Client.currentClientID++;
+        this.Id = Clients.currentClientID++;
     }
 
     
@@ -52,7 +66,7 @@ class Client
 class Loans
 {
     private int id;
-    public Client client;
+    public Clients clients;
     private double amount;
     private double rate;
     private int startRateYear;
@@ -64,10 +78,10 @@ class Loans
     public int StartRateYear { get; set; }
     public int EndRateYear { get; set; }
 
-    public Loans(Client client, double amount, double rate, int startRateYear)
+    public Loans(Clients clients, double amount, double rate, int startRateYear)
     {
         this.Id = Loans.currentLoanID++;
-        this.client = client;
+        this.clients = clients;
         this.amount = amount;
         this.rate = rate;
         this.StartRateYear = startRateYear;
